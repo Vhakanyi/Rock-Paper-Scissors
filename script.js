9a3691ebfd9d11150a11dbbf3432ea125cd91d8f
vhakanyi <156878927+Vhakanyi@users.noreply.github.com>
2025-03-25 17:48:28 +0200

    add a functiont to get a computer choice

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+// The Function to get the computer choice.
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+
+    if (randomNumber < 0.33) {
+        return "rock";
+    }else if (randomNumber <0.66) {
+        return "paper";
+    }else {
+        return "scissors";
+    }
+}
+
+// The function to get the human choice
+
+function getHumanChoice() {
+    return prompt("Enter: rock, paper or scissors.").toLowerCase();
+
+}
+
+console.log(getHumanChoice());
+
+
